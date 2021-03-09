366b1aa31a7e8870cbc9a10f52a93cd5daeb2c66
Francesco Ippolito <ipp.francesco@gmail.com>
2021-03-09 20:32:23 +0000

    expense test working

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixture/expenses';
+import database from '../../firebase/firebase'
 
 const mockExpense = {
   key: 'test-id',
@@ -12,15 +13,17 @@ const mockExpense = {
 }
 
 const createMockStore = configureMockStore([thunk]);
-jest.mock('../../firebase/firebase', () => ({
-  ref: () => ({
+
+jest.mock('../../firebase/firebase', () => (
+  {
+    ref: () => ({
       push: () => mockExpense,
       on: (string, callback) => {
         callback(mockExpense)
       },
-      once: (event) => Promise.resolve({val: mockExpense})
-  })
-}))
+      once: (event) => Promise.resolve({ val: mockExpense })
+    })
+  }))
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' });
@@ -50,13 +53,14 @@ test('should setup add expense action object with provided values', () => {
 });
 
 test('should add expense to database and store', (done) => {
+  const store = createMockStore()
   const expenseData = {
     description: 'Mouse',
     amount: 3000,
     note: 'This one is better',
     createdAt: 1000
   };
-  const store = createMockStore()
+
   store.dispatch(startAddExpense(expenseData))
 
   const actions = store.getActions();
@@ -67,41 +71,43 @@ test('should add expense to database and store', (done) => {
       ...expenseData
     }
   });
-  
+
   done()
 
-  //   return database.ref(`expenses/${actions[0].expense.id}`).once('value');
-  // }).then((snapshot) => {
-  //   expect(snapshot.val()).toEqual(expenseData);
-  //   done();
-  // });
+  database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+    expect(snapshot.val()).toEqual(expenseData);
+    done();
+  });
 });
 
-// test('should add expense with defaults to database and store', (done) => {
-//   const store = createMockStore({});
-//   const expenseDefaults = {
-//     description: '',
-//     amount: 0,
-//     note: '',
-//     createdAt: 0
-//   };
+test('should add expense to database and store', (done) => {
+  const store = createMockStore()
+  const expenseData = {
+    description: '',
+    amount: 0,
+    note: '',
+    createdAt: 0
+  };
 
-//   store.dispatch(startAddExpense({})).then(() => {
-//     const actions = store.getActions();
-//     expect(actions[0]).toEqual({
-//       type: 'ADD_EXPENSE',
-//       expense: {
-//         id: expect.any(String),
-//         ...expenseDefaults
-//       }
-//     });
+  store.dispatch(startAddExpense({}))
 
-//     return database.ref(`expenses/${actions[0].expense.id}`).once('value');
-//   }).then((snapshot) => {
-//     expect(snapshot.val()).toEqual(expenseDefaults);
-//     // done();
-//   });
-// }); 
+  const actions = store.getActions();
+  expect(actions[0]).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      id: expect.any(String),
+      ...expenseData
+    }
+  });
+
+  done()
+
+  database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+    expect(snapshot.val()).toEqual(expenseData);
+    done();
+  });
+});
+
 
 // test('should setup add expense action object with default values', () => {
 //   const action = addExpense();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });
 } else if (process.env.NODE_ENV === 'development') {
   require('dotenv').config({ path: '.env.development' });
 }
-
 
 module.exports = (env) => {
   const isProduction = env === 'production';
@@ -54,9 +53,7 @@ module.exports = (env) => {
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
-        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
@@ -68,6 +65,4 @@ module.exports = (env) => {
   };
 };
 
-
-//loader
 
